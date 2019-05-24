75b1524cd569611a9fb921f3c63e2066c519db6d
arahman1970 <ar.nj.usa@gmail.com>
2019-05-24 13:57:03 -0400

    inprocess

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,53 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String content;
+
+    private Date posteddate;
+
+    private String sentby;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getPosteddate() {
+        return posteddate;
+    }
+
+    public void setPosteddate(Date posteddate) {
+        this.posteddate = posteddate;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+}
